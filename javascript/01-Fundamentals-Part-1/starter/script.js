770f67d83504205ca8c374d31c40cfa9ec9c7a59
shaikArsalan347 <shaikarsalan347@gmail.com>
2024-09-14 16:47:13 +0530

    javascript

---
+++ javascript/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,183 @@
+/*let js = "amazing";
+if (js === "amazing") alert("javasript is USEFULL");
+
+console.log(50 + 30);
+
+let firstName = "shaik ayaz";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+let Firstname = "shaik arsalan";
+console.log(Firstname);
+*/
+
+// assignments
+
+// let country = "india";
+// // let continet = "asia";
+// let population = "1.45 billion";
+// // console.log(continet);
+// let isLand = false;
+// let language;
+// console.log(typeof isLand);
+// console.log(typeof country);
+// console.log(typeof population);
+// console.log(typeof language);
+
+// data type
+
+// let jsIsFun = true;
+// console.log(jsIsFun);
+// console.log(typeof "hhhh");
+// // console.log(typeof true);
+// console.log(typeof jsIsFun);
+
+// jsIsFun = "jsIsNotFun";
+// console.log(typeof jsIsFun);
+
+// const birthYear = 2008;
+// console.log(birthYear);
+
+// var job = "student";
+// job = "nothing";
+// console.log(job);
+
+// language = "portuguese";
+// const country = "Portugal";
+// const continent = "Europe";
+// let isIsland = false;
+// isIsland = true;
+// console.log(isIsland);
+
+// //maths operators
+// const now = 2024;
+// const ageArsalan = now - 2008;
+// const ageAyub = now - 2005;
+// const ageAyaz = now - 2010;
+// const ageAzra = now - 2012;
+// console.log(ageArsalan, ageAyub, ageAyaz, ageAzra);
+// const firstName = "shaik";
+// const lasttName = "Arsalan";
+// console.log(firstName + " " + lasttName);
+// //assignement operators
+// let x = 2 + 5 + 6; // 13
+// x += 10;
+// x *= 2;
+// x++;
+// x--;
+// console.log(x);
+// //comparison operators
+// console.log(ageAyub > 18);
+// console.log(ageAyub <= 18);
+
+// console.log(now - 2005 > now - 2008);
+
+// const now = 2024;
+// const ageArsalan = now - 2008;
+// const ageAyub = now - 2005;
+// const ageAyaz = now - 2010;
+// const ageAzra = now - 2012;
+// console.log(now - 2005 > now - 2008, now - 2010 < now - 2012);
+
+// const firstName = "shaik";
+// const lasttName = "Arsalan";
+// const birthYear = 2008;
+// const year = 2024;
+// console.log(`i'm ${firstName} ${lasttName} my age is ${year - birthYear}`);
+// console.log(`string with \n\
+//   arsalan`);
+
+// const age = 16;
+
+// if (age >= 18) {
+//   console.log("Arsalan can start driving license 🚗");
+// } else {
+//   const leftYears = 18 - age;
+//   console.log(`Arsalan is too young. so, wait for ${leftYears} years`);
+// }
+
+// const ayubBOD = 2024 - 2005;
+// const arsalanBOD = 2024 - 2005;
+// if (ayubBOD > arsalanBOD) {
+//   console.log("ayub age  is greater the arsalan's age");
+// } else {
+//   console.log("both ages are equal");
+// }
+// let n = "20" - "14" - "3" - 2 + "5";
+// console.log(n);
+
+// let age = 0;
+// if (age) {
+//   console.log("my age is 18");
+// } else {
+//   console.log("my age is less then 18");
+// }
+
+// const age = "18";
+// if (age === 18) console.log("you just became an adult :D (strict)");
+// if (age == 18) console.log("you just became an adult :D (losse)");
+// const favourite = Number(prompt("what's your favorite number"));
+// console.log(favourite);
+// if (favourite === 5) {
+//   console.log("cool! 5 its an amzaing number");
+// } else if (favourite === 9) {
+//   console.log("cool! 9 its an amzaing number");
+// } else if (favourite === 11) {
+//   console.log("cool! 11 its an amzaing number");
+// } else if (favourite === 100) {
+//   console.log("cool! 100 its an amzaing number");
+// } else {
+//   console.log(`${favourite} is cool! but not amzaing number`);
+// }
+// const day = "friday";
+
+// if (day === "monday") {
+//   console.log("start section 2");
+//   console.log("write assiginements");
+// } else if (day === "tuesday") {
+//   console.log("go to college");
+// } else if (day === "wednesday" || day === "thursday") {
+//   console.log("learn js");
+// } else if (day === "friday") {
+//   console.log("do ohter works");
+// } else if (day === "saturday" || day === "sunday") {
+//   console.log("enjoy the weekend");
+// } else {
+//   console.log("Not a valid day !");
+// }
+
+// switch (day) {
+//   case "monday":
+//     console.log("start section 2");
+//     console.log("write assiginements");
+//     break;
+//   case "tuesday":
+//     console.log("go to college");
+//     break;
+//   case "wednesday":
+//   case "thursday":
+//     console.log("learn js");
+//     break;
+//   case "friday":
+//     console.log("do ohter works");
+//     break;
+//   case "saturday":
+//   case "sunday":
+//     console.log("enjoy the weekend");
+//     break;
+//   default:
+//     console.log("Not a valid day !");
+// }
+
+// const age = 20;
+// const drink = age >= 18 ? "juice🧃" : "water💧";
+// console.log(drink);
+
+// console.log(`i like to drink ${age >= 18 ? "juice🧃" : "water💧"} `);
+
+// const bill = 40;
+// const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// console.log(
+//   `the bill was ${bill},the tip was ${tip}, the total value ${bill + tip}`
+// );
---
+++ javascript/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,284 @@
+"use strict";
+// let hasTenthfailclass = false;
+// const passTenthclass = true;
+// if (passTenthclass) hasTenthfailclass = true;
+// if (hasTenthfailclass) console.log("i can go to college");
+
+// functions
+
+// function logger() {
+//   console.log("My name is Shaik Arsalan");
+// }
+
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(mango, strawberry) {
+//   const juice = `mix juice with ${mango} mango and ${strawberry} strawberry`;
+//   return juice;
+// }
+// const mixjuice = fruitProcessor(3, 2);
+// console.log(mixjuice);
+
+// function nameProcessor(name, age) {
+//   const namemaker = `my name is ${name} my age is ${age}`;
+//   return namemaker;
+// }
+// const show = nameProcessor("ayub", 19);
+// console.log(show);
+
+// //function declaration
+// function calcage1(birthyear) {
+//   return 2024 - birthyear;
+// }
+// const age1 = calcage1(2008);
+
+// // function expression
+// const calcage2 = function (birthyear) {
+//   return 2024 - birthyear;
+// };
+// const age2 = calcage1(2008);
+// console.log(age1, age2);
+
+//arrow function
+
+// const calcage = (birthyear) => 2024 - birthyear;
+// const age = calcage(2008);
+// console.log(age);
+
+// const yearsToRetirment = (birthyear, FirstName) => {
+//   const age = 2024 - birthyear;
+//   let ageOfRetirment = 65 - age;
+//   return `${FirstName} you have more ${ageOfRetirment} years to get Retire `;
+// };
+// console.log(yearsToRetirment(2008, "Shaik Arsalan"));
+// console.log(yearsToRetirment(1959, "jack"));
+
+///////// Functions Calling Other Functions
+// function pgIncreases(Increases) {
+//   return Increases * 2;
+// }
+
+// function makingBook(longBookPg, shortBookPg) {
+//   const lgIncrease = pgIncreases(longBookPg);
+//   const smIncrease = pgIncreases(shortBookPg);
+//   const books = `A longbook contains ${lgIncrease} pages and A book shortbook contains ${smIncrease} pages`;
+//   return books;
+// }
+// console.log(makingBook(200, 150));
+
+// const calcage = function (birthYear) {
+//   return 2024 - birthYear;
+// };
+
+// const yearsToRetirment = function (birthYear, FirstName) {
+//   const age = 2024 - birthYear;
+//   let ageOtRetirment = 65 - age;
+//   if (ageOtRetirment > 0) {
+//     console.log(
+//       `${FirstName} you have more ${ageOtRetirment} years to get Retire `
+//     );
+//     return ageOtRetirment;
+//   } else {
+//     console.log(
+//       `${FirstName} you have more ${ageOtRetirment} years to get Retire`
+//     );
+//     return -1;
+//   }
+// };
+
+// // return `${FirstName} you have more ${ageOtRetirment} years to get Retire `;
+// console.log(yearsToRetirment(2008, "Shaik Arsalan"));
+// console.log(yearsToRetirment(1959, "jack"));
+
+// function fruitProcessor(apples, oranges) {
+//   const juice = `A mixjuice with ${apples} apples and ${oranges} oranges`;
+//   return juice;
+// }
+// const mixjuice = fruitProcessor(2, 3);
+// console.log(mixjuice);
+
+// Array
+// const data = ["ayub", " arsalan", " ayaz", "azra"];
+// console.log(data);
+
+// console.log(data[0]);
+// console.log(data[2]);
+// console.log(data[data.length - 5]);
+// const years = new Array(2005, 2008, 2010, 2012);
+// console.log(years[1]);
+// data[0] = "saad";
+// data[1] = "ayub";
+// data[2] = "arsalan";
+// data[3] = "ayaz";
+// data[4] = "azra";
+// console.log(data[data.length - 5]);
+// let friend = "ayaz";
+// friend = "ayub";
+// console.log(friend);
+// // const FirstName = "Shaik";
+// // const arsalan = [FirstName, "Arsalan", 2024 - 2008, "student"];
+// // console.log(new Array());
+// const calcage = function (birthYear) {
+//   return 2024 - birthYear;
+// };
+// const years = [2002, 2005, 2008, 2010, 2012];
+// const age1 = calcage(years[0]);
+// const age2 = calcage(years[1]);
+// const age3 = calcage(years[years.length - 1]);
+// console.log(age1, age2, age3);
+// const ages = [
+//   calcage(years[0]),
+//   calcage(years[1]),
+//   calcage(years[years.length - 1]),
+// ];
+// // add Element
+// console.log(ages);
+// const data = ["ayub", "arsalan", "ayaz", "azra"];
+// // data.push("saad");
+// data.unshift("saad");
+// // remove Element
+// let popped = data.pop();
+// console.log(popped);
+// console.log(data);
+// data.shift();
+// console.log(data.indexOf("ayub"));
+// console.log(data.indexOf("saad"));
+// console.log(data.includes("arsalan"));
+// console.log(data.includes("saad"));
+// if (data.includes("ayub")) {
+//   console.log(`you have a friend called ayub`);
+// } else {
+//   console.log(`your not a friend of ayub`);
+// }
+
+/////// objects/////////////////
+
+// const arsalan = {
+//   firstName: "shaik",
+//   lastName: "arsalan",
+//   age: 2024 - 2008,
+//   job: "student",
+//   friend: ["michael", "peter", "steven"],
+// };
+// console.log(arsalan);
+// console.log(arsalan.firstName);
+// const namekey = "Name";
+// console.log(arsalan["first" + namekey]);
+// console.log(arsalan["last" + namekey]);
+// const selected = prompt(
+//   "what do you want know about Arsalan? choose between firstname, lastname, age, job"
+// );
+// if (arsalan[selected]) {
+//   console.log(arsalan[selected]);
+// } else {
+//   console.log(
+//     "you seclcted wrong choose between firstname, lastname, age, job"
+//   );
+// }
+// console.log(
+//   `${arsalan.lastName} has ${arsalan.friend.length} friends, and his best friend is called ${arsalan.friend[0]}`
+// );
+/////////////object methods////////////////////////////////
+// const arsalan = {
+//   firstName: "shaik",
+//   lastName: "arsalan",
+//   birthYear: 2008,
+//   job: "student",
+//   friend: ["michael", "peter", "steven"],
+//   hasDrivingLicense: false,
+// calcAge: function (birthYear) {
+//   return 2024 - birthYear;
+// },
+// calcAge: function () {
+//   // console.log(this);
+// console.log(arsalan.calcAge());
+// console.log(arsalan.age);
+// console.log(arsalan.age);
+// console.log(arsalan.age);
+// console.log(arsalan.age);
+
+//   return 2024 - this.birthYear;
+// },
+//challenge
+//   calcAge: function () {
+//     this.age = 2024 - this.birthYear;
+//     return this.age;
+//   },
+//   getSummary: function () {
+//     return `${this.lastName} is a ${this.age} years old ${
+//       this.job
+//     } and he has ${this.hasDrivingLicense ? "a" : "no"} driver's license`;
+//   },
+// };
+// arsalan.calcAge();
+// console.log(arsalan.getSummary());
+
+//////////// for loop ///////////////////
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log(`Lifting weights repetition ${rep} 🏋️`);
+// }
+// const arsalan = [
+//   "shaik",
+//   "arsalan",
+//   2024 - 2008,
+//   "student",
+//   ["michael", "peter", "steven"],
+//   true,
+// ];
+// const types = [];
+// for (let i = 0; i < arsalan.length; i++) {
+//   console.log(arsalan[i], typeof arsalan[i]);
+//   types[i] = typeof arsalan[i];
+//   // types.push(typeof arsalan[i]);
+// }
+// console.log(types);
+
+// const years = [2005, 2008, 2010, 2012];
+// const ages = [];
+// for (let i = 0; i < years.length; i++) {
+//   ages.push(2024 - years[i]);
+// }
+// console.log(ages);
+// //// continue and break
+// console.log("--- only STRINGS ----");
+// for (let i = 0; i < arsalan.length; i++) {
+//   if (typeof arsalan[i] !== "string") continue;
+
+//   console.log(arsalan[i], typeof arsalan[i]);
+// }
+// console.log("--- only number ----");
+// for (let i = 0; i < arsalan.length; i++) {
+//   if (typeof arsalan[i] === "string") break;
+
+//   console.log(arsalan[i], typeof arsalan[i]);
+// }
+////////////// backward loop///////////////
+const arsalan = [
+  "shaik",
+  "arsalan",
+  2024 - 2008,
+  "student",
+  ["michael", "peter", "steven"],
+];
+for (let i = arsalan.length - 1; i >= 0; i--) {
+  console.log(i, arsalan[i]);
+}
+
+/////////// while loop////////
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log(`Lifting weights repetition ${rep} 🏋️`);
+// }
+// let rep = 1;
+// while (rep <= 10) {
+//   console.log(`WHILE:Lifting weights repetition ${rep} 🏋️`);
+//   rep++;
+// }
+
+// let dice = Math.trunc(Math.random() * 6) + 1;
+// while (dice !== 6) {
+//   console.log(`you got ${dice}`);
+//   dice = Math.trunc(Math.random() * 6) + 1;
+//   if (dice === 6) console.log(`loop is ended...`);
+// }
